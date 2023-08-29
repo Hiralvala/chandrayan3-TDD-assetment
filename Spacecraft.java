18aae2c851334d71a9fb61a4fd4d895785362c70
Hiral <valahiral563@gmail.com>
2023-08-29 22:43:01 +0530

    initial commit

---
+++ Spacecraft.java
@@ -0,0 +1,9 @@
+public class Spacecraft {
+    public static void main(String[] args) {
+        int initialX=0,initialY=0,initialZ=0;
+        char initialdirection='N';
+        char[]commands={'f','r','u','b','l'};
+        
+    }
+    
+}
