450dd4d0b3bee4e5743a481d74306736cdef8b87
Hiral <valahiral563@gmail.com>
2023-08-29 22:55:41 +0530

    create object and constructor ,then initialize variable

--- Spacecraft.java
+++ Spacecraft.java
@@ -1,9 +1,20 @@
 public class Spacecraft {
+    int x,y,z;
+    char direction;
+
+    Spacecraft(int initialX,int initialY,int initialZ,char initialdirection){
+        x=initialX;
+        y=initialY;
+        z=initialZ;
+        direction=initialdirection;
+    }
     public static void main(String[] args) {
         int initialX=0,initialY=0,initialZ=0;
         char initialdirection='N';
         char[]commands={'f','r','u','b','l'};
-        
+
+        Spacecraft chandrayan3=new Spacecraft(initialX,initialY,initialZ,initialdirection);
+
     }
     
 }
